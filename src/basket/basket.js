2d8031ef2d1b0b14fff559e40734c67257dfc593
Sofia Anjos <emmanuelesofia@gmail.com>
2022-06-19 18:05:50 -0300

    arrumei bugs no código

--- src/basket/basket.js
+++ src/basket/basket.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React from "react";
 
 export default function Bascket(props) {
-  const {cartItems, onAdd, onRemove} = props;
-  return (
-    <aside className='bloco1'>
-   <h2>Carrinho:</h2>
-   <div>{cartItems === 0 && <div>O carinho está vazio</div>}</div>
-   {cartItems.map((item) => (
-    <div key={item.id} className="row">
-    <div>{item.name}</div>  
-   <div>
-   <button onClick={()=>onAdd(item)} className="add">+</button>
-   <button onClick={()=>onRemove(item)} className="add">-</button>
-   </div>
-   
-   ))}
-   </aside>
+  const { cartItems, onAdd, onRemove } = props;
+  const itensCarrinho = cartItems.map((item) => {
+    return (
+      <div key={item.id}>
+        <div>{item.name}</div>
+        <div>
+          <button onClick={() => onAdd(item)} className="add">
+            +
+          </button>
+          <button onClick={() => onRemove(item)} className="add">
+            -
+          </button>
+        </div>
+      </div>
+    );
+  });
 
-    
+  return (
+    <aside className="bloco1">
+      <h2>Carrinho:</h2>
+      <div>{cartItems === 0 && <div>O carinho está vazio</div>}</div>
+      {itensCarrinho}
+    </aside>
   );
 }
